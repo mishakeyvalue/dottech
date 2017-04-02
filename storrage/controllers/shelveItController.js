77969b1b63f6226072ea250f8ecb01aaa9b258f1
mitutee <volman1285@gmail.com>
2017-04-02 18:26:50 +0300

    Initialized with the ninja-styled  view

---
+++ storrage/controllers/shelveItController.js
@@ -0,0 +1,13 @@
+module.exports = function(app) {
+    app.get('/', function(req, res) {
+        res.render('shelveIt');
+    });
+
+    app.post('/', function(req, res) {
+
+    });
+
+    app.delete('/', function(req, res) {
+
+    });
+};
