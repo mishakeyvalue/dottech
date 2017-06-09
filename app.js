baadfe02c8ccdef5a117ea20db72daa2da958fff
mitutee <volman1285@gmail.com>
2017-06-09 23:15:59 +0300

    Megna initialized

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 const express = require('express');
-const StorageController = require('./Controllers/StorageController');
 
 let app = express();
 // configure my app
 app.set('view engine', 'ejs');
-app.use('/assets', function(req, res, next) {
-    console.log("assets were asked: " + req.url)
-    express.static('public/assets')(req, res, next);
+app.use(express.static('public'))
+
+
+app.get('/', (req, res) =>{
+    res.render('index');
 });
-
-
 // Fire up Controllers
-StorageController(app);
 
-app.listen(3000);
+app.listen(5555);
