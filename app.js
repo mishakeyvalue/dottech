814cb877eb590a1acf7aa391529be922260e06d7
mitutee <volman1285@gmail.com>
2017-07-05 22:33:30 +0300

    Trash cleaned

--- app.js
+++ app.js
@@ -2,25 +2,25 @@ const express = require('express');
 
 let app = express();
 
-
-var opts = require('optimist')
-    .options({
-        port: {
-            demand: false,
-            alias: 'p',
-            description: 'port to listen to'
-        },
-    }).boolean('allow_discovery').argv;
+// Configure port via params (or default)
+var opts = require('optimist').options({
+    port: {
+        demand: false,
+        alias: 'p',
+        description: 'port to listen to'
+    },
+}).boolean('allow_discovery').argv;
 let PORT;
 if (opts.port) {
     PORT = opts.port;
 } else PORT = 5555;
+
 // configure my app
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
 // Fire up Controllers
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     res.render('index');
 });
 
