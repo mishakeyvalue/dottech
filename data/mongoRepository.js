bdaa4ff2356f2964f3ef74969cd049a74fc2d287
Mykhailo Kilianovskyi <fen1285@mail.ru>
2017-08-13 08:33:18 -0700

    New .NET Core template installed

--- data/mongoRepository.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const mongoose = require('mongoose');
-const _configManager = require('../_configManager');
-
-let _connectionString = _configManager.getConnectionString();
-mongoose.connect(_connectionString);
-let db = mongoose.connection;
-
-module.exports = db;
