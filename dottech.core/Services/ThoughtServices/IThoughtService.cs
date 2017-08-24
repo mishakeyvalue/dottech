b85cf93a0dba8a1e03072b378baaa4ef1a841f27
Mykhailo Kilianovskyi <fen1285@mail.ru>
2017-08-24 15:59:15 +0300

    ThougtService interface provided!

---
+++ dottech.core/Services/ThoughtServices/IThoughtService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using dottech.core.Models;
+using System.Collections.Generic;
+
+namespace dottech.core.Services
+{
+    public interface IThoughtService
+    {
+        ThoughtModel Create(ThoughtModel thought);
+        IEnumerable<ThoughtModel> GetAll();
+        ThoughtModel Get(Guid id);
+        ThoughtModel Update(ThoughtModel thought);
+        void Delete(Guid id);
+    }
+}
